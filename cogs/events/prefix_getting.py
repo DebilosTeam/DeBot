a41cecd83b4efb30200219eca65db08fbfdae526
OctoBanon <82901151+OctoBanon-Main@users.noreply.github.com>
2022-05-29 12:14:23 +0700

    Add files via upload

---
+++ cogs/events/prefix_getting.py
@@ -0,0 +1,26 @@
+from disnake.ext import commands
+
+DEFAULT_PREFIX = '-'
+
+
+async def get_prefix(bot, message):
+    if not message.guild:
+        return commands.when_mentioned_or(DEFAULT_PREFIX)(bot, message)
+
+    prefix = await bot.db.fetch('SELECT prefix FROM guild_prefixes WHERE "id" = $1', message.guild.id)
+    if len(prefix) == 0:
+        await bot.db.execute('INSERT INTO guild_prefixes("id", prefix) VALUES ($1, $2)', message.guild.id, DEFAULT_PREFIX)
+        prefix = DEFAULT_PREFIX
+    else:
+        prefix = prefix[0].get("prefix")
+    return commands.when_mentioned_or(prefix)(bot, message)
+
+
+class Prefix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Prefix(bot))
+    bot.loop.run_until_complete(bot.create_db_pool())
