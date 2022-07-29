47abcc0665fff695b079cb09ef1e09dc8b6fc16f
OctoBanon <octobanon@gmail.com>
2022-07-29 07:27:51 +0700

    Updated help.py

--- commands/user/help.py
+++ commands/user/help.py
@@ -1,4 +1,65 @@
+from disnake import ui, Embed, SelectOption, MessageInteraction
 from disnake.ext import commands
+
+from datetime import datetime
+
+
+class Embeds:
+    page_one = Embed(
+        title='User commands',
+        description='Here will be user commands',
+        color=0xb49dd4,
+        timestamp=datetime.utcnow()
+    )
+    page_one.set_footer(text='Copyright © Debilos Team 2022')
+
+    page_two = Embed(
+        title='Fun commands',
+        description='Here will be fun commands',
+        color=0xb49dd4,
+        timestamp=datetime.utcnow()
+    )
+    page_two.set_footer(text='Copyright © Debilos Team 2022')
+
+    page_three = Embed(
+        title='Server owner commands',
+        description='Here will be server owner commands',
+        color=0xb49dd4,
+        timestamp=datetime.utcnow()
+    )
+    page_three.set_footer(text='Copyright © Debilos Team 2022')
+
+
+class Dropdown(ui.Select):
+    def __init__(self):
+        """Its init dropdown menu for help cog only"""
+        options = [
+            SelectOption(label='User commands', description='Commands available to all'),
+            SelectOption(label='Fun commands', description='Just commands like 8ball and etc'),
+            SelectOption(label='Server owner commands', description='Commands for bot control'),
+        ]
+
+        super().__init__(
+            min_values=1,
+            max_values=1,
+            options=options
+        )
+
+    async def callback(self, interaction: MessageInteraction):
+        if self.values[0] == 'User commands':
+            await interaction.response.edit_message(embed=Embeds.page_one)
+        elif self.values[0] == 'Fun commands':
+            await interaction.response.edit_message(embed=Embeds.page_two)
+        elif self.values[0] == 'Server owner commands':
+            await interaction.response.edit_message(embed=Embeds.page_three)
+
+
+class DropdownView(ui.View):
+    def __init__(self):
+        """Adding of dropdown menu for help embed"""
+        super().__init__()
+
+        self.add_item(Dropdown())
 
 
 class Help(commands.Cog):
@@ -8,7 +69,16 @@ class Help(commands.Cog):
 
     @commands.slash_command(description='Send bot command list')
     async def help(self, inter):
-        await inter.response.send_message("GG")
+        view = DropdownView()
+
+        help_embed = Embed(title='Commands list',
+                           description='Hello, you called help command, all the bot commands will be here',
+                           color=0xb49dd4,
+                           timestamp=datetime.utcnow())
+
+        help_embed.set_footer(text='Copyright © Debilos Team 2022')
+
+        await inter.response.send_message(embed=help_embed, view=view)
 
 
 def setup(bot):
