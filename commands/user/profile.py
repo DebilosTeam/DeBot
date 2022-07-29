f44dd05f2232b5ca0f99e4a4e0c0d723ce0496c6
OctoBanon <octobanon@gmail.com>
2022-07-29 07:31:38 +0700

    Forgot add setup function in profile.py

--- commands/user/profile.py
+++ commands/user/profile.py
@@ -5,3 +5,7 @@ class Profile(commands.Cog):
     def __init__(self, bot):
         """Its cog for user command profile and can't be used in another cogs or main.py"""
         self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(Profile(bot))
